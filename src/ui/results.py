a138a508dddbd04d5c2a0243843d41d0081acc9a
Peter-L-SVK <leukanic.peter@gmail.com>
2026-06-22 16:39:50 +0200

    flake8(results.py): remove unused imports

--- src/ui/results.py
+++ src/ui/results.py
@@ -11,13 +11,12 @@
 from __future__ import annotations
 
 import logging
-import os
 
 import gi
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Vte", "3.91")
-from gi.repository import Gtk, Vte, GLib, GtkSource
+from gi.repository import Gtk, Vte, GLib
 
 from src.config import RESULTS_ROW_LIMIT
 
